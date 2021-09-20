6a98d59c6f17c77499afbf51aaea9e6d11a5fc02
vepaks <kamburoff@gmail.com>
2021-09-20 22:23:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let age = 23;
+let number = 5;
+const LeftBorderWight = 1;
+
+console.log(number);
+
+let alex = "Alex";
+let max = "Maxim";
+
+console.log(alex, max);
+
+const Poll = 21;
+
+
+
+let obj = {
+    age: 26,
+    name: "Alex",
+    singel: false,
+
+      
+};
+
+console.log (obj.singel, obj.age);
+
+//Конструкция на линкове
+console.log (`http://barierata.com/${age}/${"alex"}/${"max"}` );
+
+
+console.log (5 * age); 
+
+let arr  = true,
+     drr = false;
+console.log (arr && !drr);
+
+alert("Hello SoftUni");
+
