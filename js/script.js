b39dff6102f11837a85887a03d415fe0f66adeaf
vepaks <kamburoff@gmail.com>
2021-09-26 22:17:52 +0300

    Thirth practice complite code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 /* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+1) Первую часть задания повторить по уроку
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-4) Потренироваться и переписать цикл еще двумя способами*/
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-let numberOfFilms = +prompt ("Колко филма вече изгледахте?");
+let numberOfFilms;
+
+function start () {
+
+    numberOfFilms = +prompt ('Колко филма изгледахте?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) {
+    numberOfFilms = +prompt ('Колко филма изгледахте?', '');
+
+    }
+
+}
+
+start();
+
 
 const personalMovieDB = {
 
@@ -26,29 +39,52 @@ const personalMovieDB = {
 
 };
 
-for (let i = 0; i <2; i++){
-    let a = prompt ('Един от последните изгледани филми?', ''),
-        b = prompt ('Каква оценка му поставяте?');
-        if (a !== '' && b !== '' && a !== null && b !== null && a.length < 50 && b.length < 50 ) {
-            personalMovieDB.movies [a] = b;
-            console.log ('done');
-            } else {
-           console.log ('Error');
-           i--; 
-          }  
+function RememberMyFilms () {
+    for (let i = 0; i <2; i++){
+        let a = prompt ('Един от последните изгледани филми?', ''),
+            b = prompt ('Каква оценка му поставяте?');
+            if (a !== '' && b !== '' && a !== null && b !== null && a.length < 50 && b.length < 50 ) {
+                personalMovieDB.movies [a] = b;
+                console.log ('done');
+                } else {
+               console.log ('Error');
+               i--; 
+              }  
+    }
+    
 }
 
-if (personalMovieDB.count < 10) {
-    console.log ('Много малко филми!');
-} else if (personalMovieDB.count < 10, personalMovieDB.count > 30) {
-    console.log ('Добре');    
-} else if (personalMovieDB.count > 30 ) {
-    console.log ('Отлично!');
-} else {
-    console.log ('Грешка!');
+//RememberMyFilms ();
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log ('Много малко филми!');
+    } else if (personalMovieDB.count < 10, personalMovieDB.count > 30) {
+        console.log ('Добре');    
+    } else if (personalMovieDB.count > 30 ) {
+        console.log ('Отлично!');
+    } else {
+        console.log ('Грешка!');
+    }
+}
+//detectPersonalLevel ();
+
+function showMyDB (bou) {
+    if (!bou) {
+        console.log(personalMovieDB);
+    }
 }
 
-console.log (personalMovieDB);
+showMyDB(personalMovieDB.privat);
+
+function whriteYourGenderes () {
+        for (let i = 1; i < 4; i++){
+           personalMovieDB.genres [i - 1] = prompt ('Вашият любим жанр под номер ${1}');        
+        } 
+
+}
 
 
-// Код возьмите из предыдущего домашнего задания
+whriteYourGenderes ();
+ 
+
