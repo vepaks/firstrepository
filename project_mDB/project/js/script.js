ffd7fa7e66a0796f2e6dd0003da7374545605c95
Aleksandar Kamburov <kamburoff@gmail.com>
2021-12-05 20:11:46 +0200

    end practice 4

--- project_mDB/project/js/script.js
+++ project_mDB/project/js/script.js
@@ -24,3 +24,31 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genres = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    filmTitle = document.querySelector('.promo__interactive-title');
+
+
+adv.forEach(element => {
+    element.remove();
+});
+
+genres.textContent = 'драма';
+
+filmTitle.textContent = 'ЛЮБИМИ ФИЛМИ';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
